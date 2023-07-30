6cd68500054a6304fbed1e9e5b480686f4adae35
chmounika777 <139335504+chmounika777@users.noreply.github.com>
2023-07-30 14:51:23 +0530

    pip.tf

--- pip.tf
+++ pip.tf
@@ -1,9 +1,10 @@
-subscription_id = "48c7cd04-2b44-4873-b5aa-25b49bd06b94"
+
   provider "azurerm" {
   features {}
   client_id       = "3bdd3137-525d-412a-b875-c4362a6a0810"
   client_secret   = "hHI7Q~FX_Jrnbu1QKyOguCjExUDjY.gHIkyst"
   tenant_id       = "1b478c1a-de95-4cca-b620-81b2773ddff3"
+subscription_id = "48c7cd04-2b44-4873-b5aa-25b49bd06b94"
 }
 terraform {
   backend "azurerm" {
