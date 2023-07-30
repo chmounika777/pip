b09814457ad9c0b78df0c0da2b9ab8dccde73b9a
chmounika777 <139335504+chmounika777@users.noreply.github.com>
2023-07-30 14:53:24 +0530

    pip.tf

--- pip.tf
+++ pip.tf
@@ -22,8 +22,8 @@ data "azurerm_resource_group" "example" {
 
 resource "azurerm_public_ip" "example" {
   name                = "acceptanceTestPublicIp1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
   allocation_method   = "Static"
 
   tags = {
