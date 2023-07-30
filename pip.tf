e421697cf6be3e239347d3fb6bb5518f5c784fd1
chmounika777 <139335504+chmounika777@users.noreply.github.com>
2023-07-30 15:13:33 +0530

    pip.tf

--- pip.tf
+++ pip.tf
@@ -2,7 +2,7 @@
   provider "azurerm" {
   features {}
   client_id       = "3bdd3137-525d-412a-b875-c4362a6a0810"
-  client_secret   = "hHI7Q~FX_Jrnbu1QKyOguCjExUDjY.gHIkyst"
+  client_secret   = "5v58Q~mxlsBIcpCR5MQhlZ-P4c-zKoUxHqrI7aW4"
   tenant_id       = "1b478c1a-de95-4cca-b620-81b2773ddff3"
 subscription_id = "48c7cd04-2b44-4873-b5aa-25b49bd06b94"
 }
